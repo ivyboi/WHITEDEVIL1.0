92c21d0f145719e9563017853b1bd165751c6236
theshashankk <78837537+theshashankk@users.noreply.github.com>
2021-02-18 09:03:35 -0800

    Update __init__.py

--- userbot/wraptools/__init__.py
+++ userbot/wraptools/__init__.py
@@ -1,5 +1,5 @@
 #    Copyright (C) Midhun KM 2020
-#    This program is free software: you can redistribute it and/or modify
+#    This program is preimium software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #
